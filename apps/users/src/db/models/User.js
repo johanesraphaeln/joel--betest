2f4f9018c6d5f6b912b91b2370b90bb785128cb5
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2024-08-12 18:52:09 +0700

    add constraint and indexes in users

--- apps/users/src/db/models/User.js
+++ apps/users/src/db/models/User.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-  userName: { type: String, required: true },
-  accountNumber: { type: String, required: true },
-  emailAddress: { type: String, required: true },
-  identityNumber: { type: String, required: true },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  accountNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  emailAddress: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  identityNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 });
+
+UserSchema.index({ accountNumber: 1 });
+UserSchema.index({ identityNumber: 1 });
 
 module.exports = User = mongoose.model('user', UserSchema);
